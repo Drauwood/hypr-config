0ae65de0fa94548586c49872b5b52ced52f0c503
Drauwood <drauwood@gmail.com>
2025-03-29 23:45:29 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,15 +1,67 @@
 #!/bin/bash
 
-echo "Installing programs..."
-sudo pacman -S hyprland hyprpaper waybar firefox neovim alacritty mc cmake smplayer cmus wofi btop numlockx udiskie pamixer grim slurp feh zip unzip
+IsRoot(){
+    if [[ $EUID -ne 0 ]]; then
+        echo "This script must be run as root"
+        exit 1
+    fi
+}
 
-echo "Installing fonts..."
-sudo pacman -S ttf-liberation ttf-dejavu opendesktop-fonts ttf-bitstream-vera ttf-arphic-ukai ttf-arphic-uming ttf-hanazono ttf-jetbrains-mono-nerd
+HelloScreen(){
+    echo '==========================================================='
+    echo 'Hyprland install script'
+    echo 'Author: https://github.com/Drauwood'
+    echo '==========================================================='
+}
 
-echo "Copying settings..."
-cp -r .config ~/
-cp -r .bash_profile ~/
-cp -r .bashrc ~/
+UpdateReposytoryesDB() {
+    echo "Updating system..."
+    sudo pacman --noconfirm -Sy
+}
+
+InstallXorg() {
+    echo "Install xorg..."
+    sudo pacman --noconfirm -S xorg-server xorg-xinit xorg-xset xorg-xsetroot xorg-xrandr xorg-xrdb
+}
+
+InstallMainPackeges() {
+    echo "Install Programs..."
+    sudo pacman --noconfirm -S hyprland hyprpaper waybar firefox neovim alacritty mc cmake smplayer cmus wofi btop numlockx udiskie pamixer grim slurp feh zip unzip
+}
+
+InstallFont() {
+	echo "Install Fonts..."
+	sudo pacman --noconfirm -S ttf-liberation ttf-dejavu opendesktop-fonts ttf-bitstream-vera ttf-arphic-ukai ttf-arphic-uming ttf-hanazono ttf-jetbrains-mono-nerd
+}
+
+InstallOrUpdateBaseBasedevel() {
+    echo "Update or install base, base-devel packeges..."
+    sudo pacman --noconfirm -S base base-devel
+}
+
+CopyConfigs() {
+    cp -rf .config ~/
+    cp -rf .bash_profile ~/
+    cp -rf .bashrc ~/
+}
+
+MakeExecutable() {
+    chmod +x ~/.config/bspwm/bspwmrc
+    chmod +x ~/.config/polybar/launch.sh
+    chmod +x ~/.config/polybar/launch_additional.sh
+}
+
+#IsRoot
+HelloScreen
+UpdateReposytoryesDB
+
+#InstallXorg
+InstallMainPackeges
+InstallFont
+InstallOrUpdateBaseBasedevel
+
+CopyConfigs
+#MakeExecutable
 
 echo "Volume 100%..."
 pamixer --set-volume 100
