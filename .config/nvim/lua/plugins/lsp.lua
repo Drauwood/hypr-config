29abd0738188388721f96d8d4612bb4448f9f0d1
Drauwood <drauwood@gmail.com>
2026-01-08 04:58:55 +0200

    new

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -13,7 +13,7 @@ return {
         'williamboman/mason-lspconfig.nvim',
         config = function ()
             require('mason-lspconfig').setup({
-                ensure_installed = { 'lua_ls', 'clangd', 'rust_analyzer' }
+                ensure_installed = { 'lua_ls', 'clangd', 'rust_analyzer', 'tombi' }
             })
         end
     }
--- install.sh
+++ install.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+IsRoot() {
+    if [[ $EUID -ne 0 ]]; then
+        echo ":: This script must be run as root"
+        exit 1
+    fi
+}
+
 HelloScreen() {
     echo '==========================================================='
     echo ':: Hyprland Install Script'
@@ -13,14 +20,35 @@ UpdateReposytoryesDB() {
     sudo pacman --noconfirm -Sy
 }
 
+InstallXorg() {
+    echo ":: Install xorg..."
+    sudo pacman --noconfirm -S xorg-server xorg-xinit xorg-xset xorg-xsetroot xorg-xrandr xorg-xrdb xorg-server-devel
+}
+
 InstallMainPackeges() {
     echo ":: Install Programs..."
-    sudo pacman --noconfirm -S hyprland hyprpaper hyprshot waybar alacritty
+    sudo pacman --noconfirm -S hyprland hyprpaper hyprshot waybar firefox neovim alacritty mc cmake smplayer btop numlockx udiskie pamixer feh zip unzip transmission-gtk ripgrep fzf nodejs npm
 }
 
 InstallFont() {
     echo ":: Install Fonts..."
-    sudo pacman --noconfirm -S ttf-jetbrains-mono-nerd
+    sudo pacman --noconfirm -S ttf-liberation ttf-dejavu opendesktop-fonts ttf-bitstream-vera ttf-arphic-ukai ttf-arphic-uming ttf-hanazono ttf-jetbrains-mono-nerd
+}
+
+InstallOrUpdateBaseBasedevel() {
+    echo ":: Update or install base, base-devel packeges..."
+    sudo pacman --noconfirm -S base base-devel
+}
+
+InstallLib() {
+    echo ":: Update or install lib's packeges..."
+    sudo pacman --noconfirm -S libxcursor libxrandr libxinerama libxi libxkbcommon
+}
+
+InstallLogin() {
+    echo ":: Install GDM ..."
+    sudo pacman --noconfirm -S gdm
+    sudo systemctl enable gdm
 }
 
 CopyConfigs() {
@@ -29,10 +57,31 @@ CopyConfigs() {
     cp -rf .bashrc ~/
 }
 
+MakeExecutable() {
+    chmod +x ~/.config/polybar/launch.sh
+    chmod +x ~/.config/polybar/launch_additional.sh
+}
+
+WaybarInstall() {
+    echo ":: Copy Waybar configs ..."
+    git clone -b fix/v0.14.0 https://github.com/sejjy/mechabar.git ~/.config/waybar
+    ~/.config/waybar/install.sh
+}
+
+#IsRoot
 HelloScreen
 UpdateReposytoryesDB
 
+#InstallXorg
 InstallMainPackeges
 InstallFont
+InstallOrUpdateBaseBasedevel
+#InstallLib
+#InstallLogin
 
 CopyConfigs
+#MakeExecutable
+WaybarInstall
+
+echo ":: Volume 100%..."
+pamixer --set-volume 100
