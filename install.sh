fed76651fe88f0b4a72e8f5442ee865599d84674
Drauwood <drauwood@gmail.com>
2026-06-28 20:08:38 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+GREEN='\033[0;32m'
+RED='\033[0;31m'
+RESET='\033[0m'
+
 IsRoot() {
     if [[ $EUID -ne 0 ]]; then
         echo ":: This script must be run as root..." >> result.txt
@@ -16,45 +20,45 @@ HelloScreen() {
 }
 
 UpdateReposytoryesDB() {
-    echo ":: Updating system..." >> result.txt
+    echo ":: Updating system..."
     sudo pacman --noconfirm -Sy &&
-        echo "[ OK ] pacman --noconfirm -Sy" >> result.txt ||
-        echo "[ ERROR ] pacman --noconfirm -Sy" >> result.txt
+        echo -e "$GREEN[ ✔ ]$RESET pacman --noconfirm -Sy" >> result.txt ||
+        echo -e "$RED[ x ]$RESET pacman --noconfirm -Sy" >> result.txt
 }
 
 InstallXorg() {
-    echo ":: Install xorg..." >> result.txt
+    echo ":: Install xorg..."
     sudo pacman --noconfirm -S xorg-server xorg-xinit xorg-xset xorg-xsetroot xorg-xrandr xorg-xrdb xorg-server-devel &&
-        echo "[ OK ] Install xorg" >> result.txt ||
-        echo "[ ERROR ] Install xorg" >> result.txt
+        echo -e "$GREEN[ ✔ ]$RESET Install xorg" >> result.txt ||
+        echo -e "$RED[ x ]$RESET Install xorg" >> result.txt
 }
 
 InstallMainPackeges() {
-    echo ":: Install Programs..." >> result.txt
+    echo ":: Install Programs..."
     sudo pacman --noconfirm -S hyprland hyprpaper uwsm libnewt hyprshot waybar firefox neovim alacritty mc ranger smplayer cmake btop numlockx udiskie pamixer feh zip unzip transmission-gtk ripgrep fzf nodejs npm &&
-        echo "[ OK ] Install Programs" >> result.txt ||
-        echo "[ ERROR ] Install Programs" >> result.txt
+        echo -e "$GREEN[ ✔ ]$RESET Install Programs" >> result.txt ||
+        echo -e "$RED[ x ]$RESET Install Programs" >> result.txt
 }
 
 InstallFont() {
-    echo ":: Install Fonts..." >> result.txt
+    echo ":: Install Fonts..."
     sudo pacman --noconfirm -S ttf-liberation ttf-dejavu opendesktop-fonts ttf-bitstream-vera ttf-arphic-ukai ttf-arphic-uming ttf-jetbrains-mono-nerd &&
-        echo "[ OK ] Install Fonts" >> result.txt ||
-        echo "[ ERROR ] Install Fonts" >> result.txt
+        echo -e "$GREEN[ ✔ ]$RESET Install Fonts" >> result.txt ||
+        echo -e "$RED[ x ]$RESET Install Fonts" >> result.txt
 }
 
 InstallOrUpdateBaseBasedevel() {
-    echo ":: Update or install base, base-devel packeges..." >> result.txt
+    echo ":: Update or install base, base-devel packeges..."
     sudo pacman --noconfirm -S base base-devel &&
-        echo "[ OK ] Update or install base, base-devel packeges" >> result.txt ||
-        echo "[ ERROR ] Update or install base, base-devel packeges" >> result.txt
+        echo -e "$GREEN[ ✔ ]$RESET Update or install base, base-devel packeges" >> result.txt ||
+        echo -e "$RED[ x ]$RESET Update or install base, base-devel packeges" >> result.txt
 }
 
 InstallLib() {
-    echo ":: Update or install lib's packeges..." >> result.txt
+    echo ":: Update or install lib's packeges..."
     sudo pacman --noconfirm -S libxcursor libxrandr libxinerama libxi libxkbcommon &&
-        echo "[ OK ] Update or install lib's packeges" >> result.txt ||
-        echo "[ ERROR ] Update or install lib's packeges" >> result.txt
+        echo -e "$GREEN[ ✔ ]$RESET Update or install lib's packeges" >> result.txt ||
+        echo -e "$RED[ x ]$RESET Update or install lib's packeges" >> result.txt
 }
 
 InstallLogin() {
@@ -76,7 +80,9 @@ MakeExecutable() {
 
 NvimCopy() {
     echo ":: Copy Nvim configs..."
-    git clone https://github.com/Drauwood/nvim.git ~/.config/nvim
+    git clone https://github.com/Drauwood/nvim.git ~/.config/nvim &&
+        echo -e "$GREEN[ ✔ ]$RESET Copy Nvim configs" >> result.txt ||
+        echo -e "$RED[ x ]$RESET Copy Nvim configs" >> result.txt
 }
 
 #IsRoot
@@ -90,10 +96,11 @@ InstallOrUpdateBaseBasedevel
 #InstallLib
 #InstallLogin
 
-CopyConfigs
+CopyConfigs &&
+    echo -e "$GREEN[ ✔ ]$RESET Copy configs" >> result.txt ||
+    echo -e "$RED[ x ]$RESET Copy configs" >> result.txt
 #MakeExecutable
 NvimCopy
 
-cd
 cat result.txt
 rm result.txt
